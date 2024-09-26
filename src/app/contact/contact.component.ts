16caeee25eb351512128cdc2c28b02d82e0434d9
ManiKandan <manigandanjanakiraman@gmail.com>
2024-09-27 01:56:14 +0530

    mail trigger message correction and reactive forms implementation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,48 +1,57 @@
 import { Component, inject, Input } from '@angular/core';
-import emailjs from '@emailjs/browser'
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import emailjs from '@emailjs/browser'
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   @Input('theme') theme!: string
-  // emailForm: FormGroup
   private _snackBar = inject(MatSnackBar);
-  // constructor(private fb: FormBuilder) {
-  //   this.emailForm = this.fb.group({
-  //     name: ['', Validators.required],
-  //     mail: ['', [Validators.required, Validators.email]],
-  //     inputMessage: ['', Validators.required],
-  //   });
-  // }
 
-  sendEmail(e: Event) {
+  private formBuilder = inject(FormBuilder)
+  feedbackForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    mail: ['', Validators.required, Validators.email],
+    feedbackMessage: ''
+  })
+
+
+  sendEmail() {
     debugger
-    e.preventDefault();
-
-    // if (this.emailForm.valid) {
-    emailjs.sendForm('service_s35qag3', 'template_mcpn5hf', e.target as HTMLFormElement, '5PE-u9MQTWvErBPc7')
-      .then((result) => {
-        console.log(result.text);
-        this.openSnackBar("Thanks to Reach me")
-      }, (error) => {
-        console.error(error.text);
-        this.openSnackBar("OOPS! try after some time")
-      });
-    // }
-    // else {
-    //   this.emailForm.markAllAsTouched();
-    // }
+    const mailData = {
+      name: this.feedbackForm.value.name,
+      mail_ID: this.feedbackForm.value.mail,
+      Message: this.feedbackForm.value.feedbackMessage
+    }
+    console.warn(mailData)
+    if (this.feedbackForm.valid) {
+      emailjs.send('service_s35qag3', 'template_mcpn5hf', mailData, '5PE-u9MQTWvErBPc7')
+        .then((result) => {
+          console.log(result.text);
+          this.openSnackBar("Thanks to Reach me")
+        }, (error) => {
+          console.error(error.text);
+          this.openSnackBar("OOPS! try after some time")
+          this.feedbackForm.reset()
+        });
+    }
+    else {
+      this.feedbackForm.markAllAsTouched();
+    }
 
   }
 
 
   openSnackBar(message: string) {
-    this._snackBar.open(message, 'X');
+    this._snackBar.open(message, "Close", {
+      horizontalPosition: 'center', verticalPosition: 'top',
+    });
   }
 }
